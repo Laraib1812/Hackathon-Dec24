fc361b5424da3292b13a1e263def86ae2517e6bd
Muhammad Laraib <142295360+Laraib1812@users.noreply.github.com>
2024-12-08 21:45:01 +0500

    E-Commerce website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 export default function Home() {
   return (
-      <div>
-        <h2>hello</h2>
-      </div>
-
-    )
+    <div className="homepage">
+      <Header/>
+      <Main />
+      <Footer/>
+    </div>
+  );
 }
